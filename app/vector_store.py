9d72c09276f2dc3b9e978a8bdf8ad24f783241ee
hamzainvalid <you@example.com>
2026-05-26 18:06:42 +0300

    langchain package update

--- app/vector_store.py
+++ app/vector_store.py
@@ -5,7 +5,7 @@ from uuid import uuid4
 
 from dotenv import load_dotenv
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 from langchain_community.document_loaders import (
     DirectoryLoader,
