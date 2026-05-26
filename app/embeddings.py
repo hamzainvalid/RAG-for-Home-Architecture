83fe4c9b4080992a1448abad4b250d00fd324c28
hamzainvalid <you@example.com>
2026-05-26 22:52:51 +0300

    server created for port assigning

--- app/embeddings.py
+++ app/embeddings.py
@@ -1,7 +1,6 @@
-from langchain_community.embeddings import HuggingFaceEmbeddings
-
+from langchain_openai import OpenAIEmbeddings
 
 def get_embedding_model():
-    return HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    return OpenAIEmbeddings(
+        model="text-embedding-3-small"
     )
