aed4ae6a076786be310eff8d83a27029993a9599
hamzainvalid <you@example.com>
2026-05-26 19:50:01 +0300

    langchain import fixed

--- app/rag_service.py
+++ app/rag_service.py
@@ -1,7 +1,8 @@
 import json
 
 from langchain_openai import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
+# from langchain.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 
 from config import (
     OPENAI_API_KEY,
